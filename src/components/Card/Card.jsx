3e1ade79d9bf77ce0c61b43300aaba5d5a079b99
Oksana Taysumova <otajsumova@gmail.com>
2023-10-11 15:19:27 +0300

    correction

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,18 +6,13 @@ import style from "./style.module.scss"
 
 export default function Card(props) {
 
-    console.log(props);
-    // const [transcriptionOpen, setTranscriptionOpen] = useState(props.item.transcription);
-    // // console.log(props.item.transcription);
-    // function transcriptionClose() {
-
-    //     setTranscriptionOpen("")
-    // }
+    // console.log(props);
+    const [id, setId] = useState(0)
 
     return (
         <div>
             {props.arr.map((item, index) => (
-                < Item item={item} key={index} />
+                < Item item={item} key={index} activeCards={item.id === id} setId={setId} />
             ))}
         </div>
     );
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,14 +4,21 @@ import style from './style.module.scss'
 
 export default function Item(props) {
 
-    // console.log(props.item.transcription);
+    const classItemActive = props.activeCards ? 'transcriptionActiv' : "transcription"
+
+    function transcriptionClose() {
+        props.setId(props.item.id)
+
+
+    }
+
     return (
 
         <section className={style.card}>
             <div><h2>{props.item.english}</h2></div>
             <div><h3>{props.item.transcription}</h3></div>
-            <div><h3>{props.item.russian}</h3></div>
-            <div><button>Проверить</button></div>
+            <div><h3 className={`style.transcription ${style[classItemActive]}`}>{props.item.russian}</h3></div>
+            <div><button onClick={transcriptionClose}>Проверить</button></div>
         </section>
 
     )
